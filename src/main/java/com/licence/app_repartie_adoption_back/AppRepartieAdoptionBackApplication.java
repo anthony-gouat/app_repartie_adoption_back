03a279238092742728185fc39630e1c4f27d402f
anthony-gouat <Anthony.gouat@gmail.com>
2022-01-15 09:56:03 +0100

    first commit

---
+++ src/main/java/com/licence/app_repartie_adoption_back/AppRepartieAdoptionBackApplication.java
@@ -0,0 +1,13 @@
+package com.licence.app_repartie_adoption_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRepartieAdoptionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRepartieAdoptionBackApplication.class, args);
+    }
+
+}
